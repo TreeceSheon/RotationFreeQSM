fix matrix dimension squeezing bug, and make rotation adaptable to arbitary batch size

handy/py_rotate/rotate.py
@@ -17,16 +17,16 @@ def rotate(img: torch.Tensor, mat: torch.Tensor):
     :return: the rotated tensor.
 
     """
-    _, c, w, h, d = img.shape
+    b, _, w, h, d = img.shape
     max_dim = max(w, h, d)
 
     # cubic padding to avoid unreasonably stretching
     img = F.pad(img, [(max_dim - d) // 2, (max_dim - d) // 2, (max_dim - h) // 2,
                                                      (max_dim - h) // 2, (max_dim - w) // 2, (max_dim - w) // 2])
     # add no pure translation
-    pure_translation = torch.zeros(1, 3, 1).to(img.device)
+    pure_translation = torch.zeros(b, 3, 1).to(img.device)
 
-    affine_matrix = torch.cat([mat.squeeze(0), pure_translation], dim=2)
+    affine_matrix = torch.cat([mat.squeeze(1), pure_translation], dim=2)
     grid = F.affine_grid(affine_matrix, img.shape, align_corners=False)
     
     return F.grid_sample(input=img, grid=grid, mode='bilinear')

main.py
@@ -1,41 +1,63 @@
+import argparse
+from importlib import import_module
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from networks.hybrid import Hybrid
-from networks.unet import Unet
-from networks.resnet import DeepResNet
 from pathlib import Path
 from dataset.TFIDataset import TFIDataset
 import time
 
-batch_size = 24
-model_name = 'Hybrid'
-device = torch.device('cuda')
-path = Path('/scratch/itee/uqlguo3/data/')
-file = 'augmented_data.txt'
-dataloader = DataLoader(TFIDataset(path, file), shuffle=True, batch_size=batch_size, drop_last=True)
-print('training data size: ' + str(len(dataloader) * batch_size))
 
-model = nn.DataParallel(Hybrid(Unet(4, 16), DeepResNet(1, 1)).to(device))
-optimizer = torch.optim.Adam(model.parameters(), lr=8e-6, betas=(0.5, 0.999), eps=1e-9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=25, gamma=0.5)
-criterion = nn.MSELoss(reduction='sum')
+def main(args):
+    print(args.is_hybrid)
+    batch_size = args.batch_size
+    device = torch.device('cuda')
+    path = Path('/scratch/itee/uqlguo3/data/')
+    file = 'augmented_data.txt'
+    # dataloader = DataLoader(TFIDataset(path, file), shuffle=True, batch_size=batch_size, drop_last=True)
+    # print('training data size: ' + str(len(dataloader) * batch_size))
+    if args.is_hybrid:
+        model_name = 'hybrid'
+        model_dipole_inv = getattr(import_module('networks.unet'), 'Unet')(4, 16)
+        deblur_model = args.deblur_model
+        deblur_model = getattr(import_module('network.' + deblur_model.lower()), deblur_model)
+        model = nn.DataParallel(Hybrid(model_dipole_inv, deblur_model).to(device))
+    else:
+        model_name = args.model
+        model = nn.DataParallel(eval(args.model)())
+    optimizer = torch.optim.Adam(model.parameters(), lr=8e-6, betas=(0.5, 0.999), eps=1e-9, weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=25, gamma=0.5)
+    criterion = nn.MSELoss(reduction='sum')
 
-start_time = time.time()
+    start_time = time.time()
 
-for epoch in range(1, 201):
-    for batch_no, values in enumerate(dataloader):
-        phi, chi, rot, inv_rot, data_type = values
-        pred = model(phi, rot, inv_rot, data_type)
-        loss = criterion(pred, chi)
-        loss.backward()
-        optimizer.step()
-        if batch_no % 40 == 0:
-            print({'epoch': epoch, 'batch_no': batch_no, 'lr_rate': optimizer.param_groups[0]['lr'],
-                   'loss': loss.item(), 'time': int(time.time() - start_time)})
-    scheduler.step()
-    if epoch % 50 == 0:
-        torch.save({'optim': optimizer.state_dict(),
-                    'scheduler': scheduler.state_dict(),
-                    'model_state': model.state_dict()}, model_name + '_' + str(epoch) + '.pkl'
-                   )
+    for epoch in range(1, 201):
+        for batch_no, values in enumerate(dataloader):
+            phi, chi, rot, inv_rot, data_type = values
+            pred = model(phi, rot, inv_rot, data_type)
+            loss = criterion(pred, chi)
+            loss.backward()
+            optimizer.step()
+            if batch_no % 40 == 0:
+                print({'epoch': epoch, 'batch_no': batch_no, 'lr_rate': optimizer.param_groups[0]['lr'],
+                       'loss': loss.item(), 'time': int(time.time() - start_time)})
+        scheduler.step()
+        if epoch % 50 == 0:
+            torch.save({'optim': optimizer.state_dict(),
+                        'scheduler': scheduler.state_dict(),
+                        'model_state': model.state_dict()}, model_name + '_' + str(epoch) + '.pkl'
+                       )
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Rotation-free QSM')
+    parser.add_argument('--is-hybrid', action='store_true', default=False)
+    parser.add_argument('--model', default='Unet', choices=['Unet', 'LPCNN'])
+    parser.add_argument('--deblur-model', default='ResNet', choices=['LPCNN', 'Unet', 'PreNet3D', 'ResNet'])
+    parser.add_argument('--batch-size', default=24, type=int)
+    parser.add_argument('--epoch', default=100, type=int)
+    args = parser.parse_args()
+
+    main(args)

networks/hybrid.py
@@ -1,7 +1,5 @@
-from unet import Unet
-from resnet import DeepResNet
 import torch.nn as nn
-from handy.py_rotate import rotate
+from handy.py_rotate.rotate import rotate
 
 
 class Hybrid(nn.Module):

networks/lpcnn.py
@@ -19,10 +19,8 @@ class wBasicBlock(nn.Module):
 
 
 class LPCNN(nn.Module):
-    def __init__(self, gt_mean, gt_std):
+    def __init__(self):
         super().__init__()
-        self.gt_mean = gt_mean
-        self.gt_std = gt_std
 
         self.iter_num = 3
 
@@ -70,8 +68,8 @@ class LPCNN(nn.Module):
                         0, dim3 - z_dim, 0, dim2 - y_dim, 0,
                         dim1 - x_dim)), dim=[2, 3, 4]), dim=[2, 3, 4]))[:, :, :x_dim, :y_dim, :z_dim]
 
-            x_input = ((pn_x_pred - self.gt_mean) / self.gt_std) * mask
+            x_input = pn_x_pred * mask
             x_pred = self.gen(x_input)
-            den_x_pred = ((x_pred * self.gt_std) + self.gt_mean) * mask
+            den_x_pred = x_pred * mask
 
         return x_pred

networks/resnet.py
@@ -2,16 +2,16 @@ import torch
 import torch.nn as nn
 
 
-class DeepResNet(nn.Module):
-    def __init__(self, num_in, num_out, num_Ch=16, HG_depth=4):
-        super(DeepResNet, self).__init__()
+class ResNet(nn.Module):
+    def __init__(self, num_Ch=16, HG_depth=4):
+        super(ResNet, self).__init__()
         self.input_layer = nn.Sequential(
-            nn.Conv3d(num_in, num_Ch, 3, padding=1),
+            nn.Conv3d(1, 1, 3, padding=1),
             nn.BatchNorm3d(num_Ch),
             nn.ReLU(inplace=True),
         )
 
-        self.HG_depth = HG_depth
+        self.deepresnet = HG_depth
         self.MidLayers = []
 
         temp = list(range(1, HG_depth + 1))
@@ -21,12 +21,12 @@ class DeepResNet(nn.Module):
 
         self.MidLayers = nn.ModuleList(self.MidLayers)
 
-        self.output_layer = nn.Conv3d(num_Ch, num_out, 1, stride=1, padding=0)
+        self.output_layer = nn.Conv3d(num_Ch, 1, 1, stride=1, padding=0)
 
     def forward(self, x):
         INPUT = x
         x = self.input_layer(x)
-        temp = list(range(1, self.HG_depth + 1))
+        temp = list(range(1, self.deepresnet + 1))
         for encodingLayer in temp:
             temp_conv = self.MidLayers[encodingLayer - 1]
             x = temp_conv(x)

networks/unet.py
@@ -50,7 +50,7 @@ class Unet(nn.Module):
             res = self._output(mid)
             return res
 
-    def __init__(self, depth, base):
+    def __init__(self, depth=4, base=16):
         super(Unet, self).__init__()
         self.depth = depth
         self._input = Unet.Encoder(1, base)